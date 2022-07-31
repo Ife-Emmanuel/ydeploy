f0abef9490891dc81e18aa8ada78b78f036d8a37
emmanuelifeoluwa369@gmail.com <emmanuelifeoluwa369@gmail.com>
2022-07-31 16:14:22 +0100

    commit for resolved deployment issue where production build fails due to react-scripts webpack config file present inside the node modules with no polyfill for certain node core modules, hence was overwritten by a config-overrides.js file and now a react-rewired script for start, build and test

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+ 
+const webpack = require('webpack'); 
+module.exports = function override(config) { 
+		const fallback = config.resolve.fallback || {}; 
+		Object.assign(fallback, { 
+    	"fs": false,
+        "path": false,
+        "url": false,
+        "buffer": false,
+        "process/browser": false 
+
+      }) 
+   config.resolve.fallback = fallback; 
+   config.plugins = (config.plugins || []).concat([ 
+   	new webpack.ProvidePlugin({ 
+    	process: 'process/browser', 
+      Buffer: ['buffer', 'Buffer'] 
+    }) 
+   ]) 
+   return config; }
